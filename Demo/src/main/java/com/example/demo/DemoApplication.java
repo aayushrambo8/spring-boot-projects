af0aaefd9aa8f9fa58b420ea5b6ae9a0114d7bd5
Aayush Babu <aayushrambo8@users.noreply.github.com>
2026-06-28 17:41:38 +0530

    feat(demo): add hello REST endpoint and update DemoApplication
    - Annotate HelloController with @RestController
    - Add GET /hello mapping returning "Hello World!" HTML header
    - Clean up console output and add controller comments in DemoApplication

--- Demo/src/main/java/com/example/demo/DemoApplication.java
+++ Demo/src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,8 @@ public class DemoApplication
         public static void main(String[] args)
             {
                 SpringApplication.run(DemoApplication.class, args);
-                System.out.println("Hello World!");
+//                HelloController controller = new HelloController();
+//                String s = controller.hello();
+//                System.out.println(s);
             }
     }
--- Demo/src/main/java/com/example/demo/HelloController.java
+++ Demo/src/main/java/com/example/demo/HelloController.java
@@ -1,5 +1,14 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController
     {
+        @GetMapping("hello")
+        public String hello()
+            {
+                return "<h1>Hello World!</h1>";
+            }
     }
