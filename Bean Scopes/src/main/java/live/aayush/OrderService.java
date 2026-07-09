dc8b4c1b320f5c463f0712c217ec661968b9fe05
Aayush Babu <aayushrambo8@users.noreply.github.com>
2026-07-09 21:41:21 +0530

    feat(bean-scopes): add commented-out singleton scope reference for clarity

--- Bean Scopes/src/main/java/live/aayush/OrderService.java
+++ Bean Scopes/src/main/java/live/aayush/OrderService.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+//@Scope("singleton")
 @Scope("prototype")
 public class OrderService
 {
