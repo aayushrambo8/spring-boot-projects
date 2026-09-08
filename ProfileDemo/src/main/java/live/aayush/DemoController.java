bc6d7174d3ce891432e8a8d0df8c4663466ac311
Aayush Babu <aayushrambo8@users.noreply.github.com>
2026-09-08 12:49:19 +0530

    feat: add environment-specific configuration and basic controller
    - Added multiple `application-{profile}.properties` for environment-specific configurations.
    - Implemented `DemoController` with a `/greet` endpoint to serve welcome messages.
    - Updated `Main` to initialize Spring Boot application.
    - Set Java version to 23 in `pom.xml`.

---
+++ ProfileDemo/src/main/java/live/aayush/DemoController.java
@@ -0,0 +1,21 @@
+package live.aayush;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/demo")
+public class DemoController
+{
+    @Value("${app.message.welcome}")
+    private String message;
+
+    @GetMapping("/greet")
+    public ResponseEntity<String> greet()
+    {
+        return ResponseEntity.ok(message);
+    }
+}
